d68dc1cb4f69913d04bdebd3e4c2f19a071989d3
Rob Allen <rob@akrabat.com>
2018-07-03 08:16:57 +0100

    Remove base64 encoding of JSON
    OpenWhisk can now accept a dictionary and will convert to JSON itself.

--- actions/common/jsonResponse.swift
+++ actions/common/jsonResponse.swift
@@ -1,13 +1,9 @@
 func jsonResponse(_ data: [String:Any], code: Int = 200, headers: [String:String] = [:]) -> [String:Any] {
     var headers = headers
     headers["Content-Type"] = "application/json"
-    headers["Access-Control-Allow-Origin"] = "*"
-
-    let json = WhiskJsonUtils.dictionaryToJsonString(jsonDict: data) ?? ""
-    let body = Data(json.utf8).base64EncodedString()
 
     return [
-        "body": body,
+        "body": data,
         "statusCode": code,
         "headers": headers,
     ]
